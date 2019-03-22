5cb76f5063f7eda0814cd5a6b265da5c7fded16f
dimilorrain <dimilorrain@gmail.com>
2019-03-22 21:38:41 +0100

    en cours : reprendre une partie (sql)

--- tpfinalweb.sql
+++ tpfinalweb.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.3
+-- version 4.8.4
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le :  Dim 17 mars 2019 à 16:12
--- Version du serveur :  5.7.23
--- Version de PHP :  7.2.10
+-- Généré le :  ven. 22 mars 2019 à 20:37
+-- Version du serveur :  5.7.24
+-- Version de PHP :  7.2.14
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -21,6 +21,34 @@ SET time_zone = "+00:00";
 --
 -- Base de données :  `tpfinalweb`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `partie`
+--
+
+DROP TABLE IF EXISTS `partie`;
+CREATE TABLE IF NOT EXISTS `partie` (
+  `id` int(11) NOT NULL,
+  `token` varchar(255) NOT NULL,
+  `enCours` tinyint(1) NOT NULL,
+  `NumPhoto` int(11) NOT NULL,
+  `time` time NOT NULL,
+  `Score` int(11) NOT NULL,
+  `idPlayer` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_id_player` (`idPlayer`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Déchargement des données de la table `partie`
+--
+
+INSERT INTO `partie` (`id`, `token`, `enCours`, `NumPhoto`, `time`, `Score`, `idPlayer`) VALUES
+(1, '1dfggdfs5qsf549qsdf68493qsf35', 1, 4, '00:25:00', 15, 1),
+(2, '5dfd64qfq6468eqzf6', 0, 10, '00:56:00', 50, 1),
+(3, 'sdfgklj5qsf654fqfsdqg6', 1, 7, '00:48:00', 35, 1);
 
 -- --------------------------------------------------------
 
@@ -54,17 +82,19 @@ INSERT INTO `player` (`id`, `Name`, `Mdp`, `EnCours`, `scoreEnCours`) VALUES
 DROP TABLE IF EXISTS `score`;
 CREATE TABLE IF NOT EXISTS `score` (
   `id` int(11) NOT NULL,
+  `idPlayer` int(11) NOT NULL,
   `score` int(11) NOT NULL,
-  KEY `fk_id_Player` (`id`)
+  PRIMARY KEY (`id`),
+  KEY `fk_id_Player` (`idPlayer`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Déchargement des données de la table `score`
 --
 
-INSERT INTO `score` (`id`, `score`) VALUES
-(1, 555),
-(1, 333);
+INSERT INTO `score` (`id`, `idPlayer`, `score`) VALUES
+(1, 1, 555),
+(2, 1, 333);
 
 --
 -- Contraintes pour les tables déchargées
@@ -74,7 +104,7 @@ INSERT INTO `score` (`id`, `score`) VALUES
 -- Contraintes pour la table `score`
 --
 ALTER TABLE `score`
-  ADD CONSTRAINT `fk_id_Player` FOREIGN KEY (`id`) REFERENCES `player` (`id`);
+  ADD CONSTRAINT `fk_id_Player` FOREIGN KEY (`idPlayer`) REFERENCES `player` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
